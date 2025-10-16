0d7e7e78f3a554b8a8f1219f8734543295f913af
Nora Blase <nora.blase@alumni.fh-aachen.de>
2025-10-16 09:35:14 +0200

    updated: selection

--- qkan/selection/application_dialog.py
+++ qkan/selection/application_dialog.py
@@ -43,16 +43,15 @@ class _Dialog(QDialog):
 
 class SelectionDialog(_Dialog, SELECT_CLASS):  # type: ignore
     #button_box: QDialogButtonBox
-    radioButton: QRadioButton
-    radioButton_2: QRadioButton
-    radioButton_3: QRadioButton
-    radioButton_4: QRadioButton
-    radioButton_5: QRadioButton
-    radioButton_6: QRadioButton
-    radioButton_7: QRadioButton
-    radioButton_8: QRadioButton
-    radioButton_9: QRadioButton
-    radioButton_10: QRadioButton
+    cb_selectFlaechen: QCheckBox
+    cb_selectHaltungen: QCheckBox
+    cb_selectSchaechte: QCheckBox
+    pushButton: QPushButton
+    pushButton_2: QPushButton
+    pushButton_3: QPushButton
+    pushButton_4: QPushButton
+    pushButton_5: QPushButton
+
 
 
     def __init__(self,
@@ -62,3 +61,15 @@ class SelectionDialog(_Dialog, SELECT_CLASS):  # type: ignore
     ):
         super().__init__(default_dir, tr, parent)
 
+        self.geklickter_button = None
+
+        # Signale verbinden
+        self.pushButton_2.clicked.connect(lambda: self.button_clicked("Teilgebiet"))
+        self.pushButton_3.clicked.connect(lambda: self.button_clicked("unterhalb"))
+        self.pushButton_4.clicked.connect(lambda: self.button_clicked("kuerzester"))
+        self.pushButton.clicked.connect(lambda: self.button_clicked("oberhalb"))
+        self.pushButton_5.clicked.connect(lambda: self.button_clicked("Fliessweg"))
+
+    def button_clicked(self, name):
+        self.geklickter_button = name
+        self.accept()
